b7f6b94567beb5fb9b13e416acba1351f2e938d7
Tristan <tristanchia@gmail.com>
2025-03-12 02:35:18 +0000

    Completed Q3.2 stack_rpn

--- part-1/stack_rpn.hpp
+++ part-1/stack_rpn.hpp
@@ -5,6 +5,66 @@
 #include <cassert>
 #include <functional>
 
-// WRITE YOUR CODE HERE
+template <typename T>
+void plus(Stack<T> &stack)
+{
+    auto a = stack.top();
+    stack.pop();
+    auto b = stack.top();
+    stack.pop();
+    stack.push(a + b);
+}
+
+template <typename T>
+void minus(Stack<T> &stack)
+{
+    auto a = stack.top();
+    stack.pop();
+    auto b = stack.top();
+    stack.pop();
+    stack.push(a - b);
+}
+
+template <typename T>
+void multiplies(Stack<T> &stack)
+{
+    auto a = stack.top();
+    stack.pop();
+    auto b = stack.top();
+    stack.pop();
+    stack.push(a * b);
+}
+
+template <typename T>
+void divides(Stack<T> &stack)
+{
+    auto a = stack.top();
+    stack.pop();
+    auto b = stack.top();
+    stack.pop();
+    stack.push(a / b);
+}
+
+template <typename T>
+void negate(Stack<T> &stack)
+{
+    auto a = stack.top();
+    stack.pop();
+    stack.push(-a);
+}
+
+template <typename T>
+Stack<T> &operator<<(Stack<T> &stack, const T &value)
+{
+    stack.push(value);
+    return stack;
+}
+
+template <typename T>
+Stack<T> &operator<<(Stack<T> &stack, void (*func)(Stack<T> &))
+{
+    func(stack);
+    return stack;
+}
 
 #endif // __stack_rpn__
