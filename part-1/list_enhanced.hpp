0f9872512836460ab8996dae5e1feaa205e0590d
Tristan <tristanchia@gmail.com>
2025-03-12 05:00:33 +0000

    Completed Q5 list_enhanced

--- part-1/list_enhanced.hpp
+++ part-1/list_enhanced.hpp
@@ -7,7 +7,20 @@
 
 template <typename T> void list_delete_after(Node<T> *node)
 {
-    // WRITE YOUR CODE HERE
+    auto last_node = node;
+
+    while (node->next) {
+        node->value = node->next->value;
+        node = node->next.get();
+    }
+
+    // Delete the last node
+    if (last_node && last_node->next) {
+        while (last_node->next->next) {
+            last_node = last_node->next.get();
+        }
+        last_node->next = nullptr;
+    }
 }
 
 #endif // __list_enhanced__
